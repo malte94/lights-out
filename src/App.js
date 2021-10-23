47845c61def6a1c981e27e0b3c3b9efc9d81b7e0
Malte Hoch <mhoch94@gmail.com>
2021-10-23 22:03:04 +0200

    Board.js: Converted to Functional Component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Board />
+        <Board nrows="5" ncols="5" />
       </div>
     );
   }
--- src/Board.js
+++ src/Board.js
@@ -1,43 +1,39 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import Cell from "./Cell";
 
-class Board extends Component {
+function Board(props) {
 
-  static defaultProps = {
-    nrows: 5,
-    ncols: 5,
-    chanceLightStartsOn: 0.25
-  }
+  const [rerender, setRerender] = useState(false);
+  const [hasWon, setHasWon] = useState(false);
+  let chanceLightStartsOn = 0.25;
 
-  state = {
-    hasWon: false,
-    board: this.createBoard(),
-  }
-
-  createBoard() {
+  const createBoard = () => {
     let board = [];
-    for (let y = 0; y < this.props.nrows; y++) {
+    for (let y = 0; y < props.nrows; y++) {
       let row = [];
-      for (let x = 0; x < this.props.ncols; x++) {
-        row.push(Math.random() < this.props.chanceLightStartsOn) 
+      for (let x = 0; x < props.ncols; x++) {
+        row.push(Math.random() < chanceLightStartsOn) 
         // returns true or false if Math.Random() generated a number less or higher than chanceLightStartsOn
       }
       board.push(row);
     }
+    console.log(board); 
     return board
   }
 
-  renderBoard() {
+  const [board, setBoard] = useState(createBoard());
+
+  const renderBoard = () => {
     let tblBoard = [];
-    for(let y = 0; y < this.props.nrows; y++) {
+    for(let y = 0; y < props.nrows; y++) {
       let row = [];
-      for(let x = 0; x < this.props.ncols; x++) {
+      for(let x = 0; x < props.ncols; x++) {
         let coord = `${y}-${x}`;
         row.push(
         <Cell 
           key={coord} 
-          isLit={this.state.board[y][x]}
-          flipCellsAroundMe={() => this.flipCellsAround(coord)}
+          isLit={board[y][x]}
+          flipCellsAroundMe={() => flipCellsAround(coord)}
         />)
       }
       tblBoard.push(<tr key={y}>{row}</tr>)
@@ -45,15 +41,15 @@ class Board extends Component {
     return tblBoard;
   }
   
-  flipCellsAround(coord) {
-    let {ncols, nrows} = this.props;
-    let board = this.state.board;
+  const flipCellsAround = (coord) => {
+    console.log("Clicked: " + coord)
+    let tmpBoard = board;
     let [y, x] = coord.split("-").map(Number);
-
+    console.log("y: " + y + " x: " + x);
 
     function flipCell(y, x) {
-      if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
-        board[y][x] = !board[y][x];
+      if (x >= 0 && x < props.ncols && y >= 0 && y < props.nrows) {
+        tmpBoard[y][x] = !tmpBoard[y][x];
       }
     }
 
@@ -65,25 +61,23 @@ class Board extends Component {
     flipCell(y - 1, x);
     flipCell(y + 1, x);
 
+    setHasWon(false);
 
-    let hasWon = false;
+    console.log(board);
 
-    this.setState({board: board, hasWon: hasWon});
+    setBoard(board, tmpBoard); setRerender(!rerender);
 
   }
 
-  render() {
-    return(
-      <div className="Board">
-        <table className="Board">
-          <tbody>
-            {this.renderBoard()}
-          </tbody>
-        </table>
-      </div>
-    )
-  }
+  return(
+    <div className="Board">
+      <table className="Board">
+        <tbody>
+          {renderBoard()}
+        </tbody>
+      </table>
+    </div>
+  )
 }
-
 
 export default Board;
